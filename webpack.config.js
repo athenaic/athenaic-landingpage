13a86ff7dfc0e580f0d430fe0e449d9926b1656b
Doan Trung Hieu <dthieu1302@gmail.com>
2022-03-24 21:34:22 +0700

    Config webpack and rearrange folder structure
    Config webpack and

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const glob = require('glob');
+const webpack = require('webpack');
+const MomentLocalsPlugin = require('moment-locales-webpack-plugin');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const SRC_DIR = path.resolve(__dirname, 'src/assets/js');
+const OUT_DIR = path.resolve(__dirname, 'dist');
+
+const entryPoint = glob.sync(SRC_DIR + '/**.js').reduce(function(obj, el){
+    obj[path.parse(el).name] = el;
+    return obj;
+}, {});
+
 module.exports = {
+    entry: entryPoint,
+    devtool: 'inline-source-map',
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: OUT_DIR,
         filename: '[name].bundle.js',
+        sourceMapFilename: '[name].map'
     },
     module: {
         rules: [
@@ -24,9 +41,24 @@ module.exports = {
         ],
     },
     plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css?[contenthash]',
+            chunkFilename: '[name]/[id].css'
+        }),
+        new MomentLocalsPlugin({
+            localesToKeep: ['vi-vn']
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'global.jQuery': 'jquery',
+            'window.jQuery': 'jquery',
+            jQueryUi: 'jQueryUi',
+            _: 'lodash',
+        }),
         new HtmlWebpackPlugin({
             title: 'Home',
-            template: path.resolve(__dirname, './views/landingpage.html'), // template file
+            template: path.resolve(__dirname, 'src/views/landingpage.html'), // template file
             filename: 'index.html', // output file
         }),
         new CleanWebpackPlugin(),
